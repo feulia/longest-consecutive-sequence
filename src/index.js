b50dccf781fa2636e54e3ff779266a6da4e27a17
Yulia <43168694+feulia@users.noreply.github.com>
2018-10-03 18:41:03 +0300

    Get longest consecutive

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  
+  array.sort((a, b) => {
+    return a - b;
+  });
+
+  array = array.filter((el, i, tempArray) => {
+    if (el === tempArray[i + 1]) {
+      return 0;
+    } else {
+      return 1;
+    }
+  });
+
+  var count = 0;
+  if (array.length) {
+    count = 1;
+  }
+  var arr = [count];
+
+  array.forEach((el, i, tempArray) => {
+    if (tempArray[i + 1] - el !== 1) {
+      arr.push(count);
+      count = 1;
+    } else {
+      count++;
+    }
+  });
+
+  function getLongestConsecutive(arrayNumber) {
+    return Math.max.apply(null, arrayNumber);
+  }
+
+  return getLongestConsecutive(arr);
 }
